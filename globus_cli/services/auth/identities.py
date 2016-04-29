646b1e8e2490938146a58a5061f271fc9d1fe391
Stephen Rosen <sirosen@uchicago.edu>
2016-04-29 15:42:18 -0500

    Fix typo in auth get-identities
    Setting ids to the username set results in failures. Correct the used
    argument

--- globus_cli/services/auth/identities.py
+++ globus_cli/services/auth/identities.py
@@ -28,7 +28,7 @@ def get_identities(args):
     if args.usernames:
         params['usernames'] = ','.join(args.usernames)
     if args.identities:
-        params['ids'] = ','.join(args.usernames)
+        params['ids'] = ','.join(args.identities)
 
     res = client.get_identities(**params)
 
