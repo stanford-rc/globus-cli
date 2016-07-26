8d79a73c9a511a611178d91ad80c9e28ca13ada8
Stephen Rosen <sirosen@uchicago.edu>
2016-07-26 16:16:22 -0500

    Add batchmode to transfer async-delete
    Batch input to async-delete is processed the same exact way that input
    to async-transfer is, but with a different set of arguments and options.
    In the case of deletes, it's actually just a single argument and no
    options, but it's nice to use the same structure for both.
    Adds a transfer helper method that generalizes that line-parsing
    structure for use in both methods.
    
    Resolves #4

--- globus_cli/services/transfer/helpers.py
+++ globus_cli/services/transfer/helpers.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import click
 import json
+import sys
+import shlex
 
 from globus_sdk import TransferClient
 
@@ -72,3 +74,34 @@ def endpoint_id_option(*args, **kwargs):
         def inner_decorator(f):
             return decorate(f, **kwargs)
         return inner_decorator
+
+
+def shlex_process_stdin(process_command, helptext):
+    """
+    Use shlex to process stdin line-by-line.
+    Also prints help text.
+
+    Requires that @process_command be a Click command object, used for
+    processing single lines of input. helptext is prepended to the standard
+    message printed to interactive sessions.
+    """
+    # if input is interactive, print help to stderr
+    if sys.stdin.isatty():
+        print(('{}\n'.format(helptext) +
+               'Lines are split with shlex in POSIX mode: '
+               'https://docs.python.org/library/shlex.html#parsing-rules\n'
+               'Terminate input with Ctrl+D or <EOF>\n'), file=sys.stderr)
+
+    # use readlines() rather than implicit file read line looping to force
+    # python to properly capture EOF (otherwise, EOF acts as a flush and
+    # things get weird)
+    for line in sys.stdin.readlines():
+        # get the argument vector:
+        # do a shlex split to handle quoted paths with spaces in them
+        # also lets us have comments with #
+        argv = shlex.split(line, comments=True)
+        if argv:
+            try:
+                process_command.main(args=argv)
+            except SystemExit:
+                pass
