812bff299ec478b8cedd591f0aad5df96b09cda4
Stephen Rosen <sirosen@uchicago.edu>
2016-08-08 15:19:26 -0500

    Move handling for main func to globus_cli.parsing
    There's a lot of custom handling for the CLI's main function,
    particularly surrounding its specialized subclass of click.Group, used
    to handle exceptions with our custom excepthook. Moving this
    functionality into one bin for tidiness keeps all of the ugly mess well
    isolated.

---
+++ globus_cli/parsing/__init__.py
@@ -0,0 +1,5 @@
+from globus_cli.parsing.main_command_decorator import globus_main_func
+
+__all__ = [
+    'globus_main_func'
+]
--- globus_cli/run.py
+++ globus_cli/run.py
@@ -1,32 +1,14 @@
 from __future__ import print_function
 import click
-import sys
 
-from globus_cli.excepthook import custom_except_hook
+from globus_cli.parsing import globus_main_func
+
 from globus_cli.list_commands import list_commands
 from globus_cli.config_command import config_command
 from globus_cli.helpers import common_options
 
 from globus_cli.services.auth import auth_command
 from globus_cli.services.transfer import transfer_command
-
-
-# there is a single global object for all click contexts, a shared dict
-_global_click_obj = {}
-
-
-class TopLevelGroup(click.Group):
-    """
-    This is a custom command type which is basically a click.Group, but is
-    designed specifically for the top level command.
-    It's specialization is that it catches all exceptions from subcommands and
-    passes them to a custom error handler.
-    """
-    def invoke(self, ctx):
-        try:
-            return super(TopLevelGroup, self).invoke(ctx)
-        except Exception:
-            custom_except_hook(sys.exc_info())
 
 
 @click.command('login', short_help='Show Login Help for the CLI',
@@ -40,8 +22,7 @@ def login_help():
     print('and select the the "Globus CLI" option.\n')
 
 
-@click.group('globus', context_settings=dict(obj=_global_click_obj),
-             cls=TopLevelGroup)
+@globus_main_func
 @common_options
 def main():
     pass
