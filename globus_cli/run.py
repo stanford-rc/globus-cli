9a239c993502e3f317edd478a5d8b5f225c24b18
Stephen Rosen <sirosen@uchicago.edu>
2016-09-17 11:58:32 -0500

    Fix doubled-help on main command
    Main command was being doubly decorated with the common options. As a
    result, it had funky looking helptext.

--- globus_cli/run.py
+++ globus_cli/run.py
@@ -3,14 +3,12 @@ from globus_cli.parsing import globus_main_func
 from globus_cli.login import login_command
 from globus_cli.list_commands import list_commands
 from globus_cli.config_command import config_command
-from globus_cli.helpers import common_options
 
 from globus_cli.services.auth import auth_command
 from globus_cli.services.transfer import transfer_command
 
 
 @globus_main_func
-@common_options
 def main():
     pass
 
