57480f43bf9b6b01120bb63d1155a4a11aaa77ab
Dave Shifflett <dave@corpulentcoffee.com>
2016-12-09 10:36:10 -0600

    Fallback to cname on error msg if no display name

--- globus_cli/commands/endpoint/server/list.py
+++ globus_cli/commands/endpoint/server/list.py
@@ -6,7 +6,8 @@ from globus_cli.safeio import safeprint
 from globus_cli.parsing import common_options, endpoint_id_arg
 from globus_cli.helpers import (colon_formatted_print, print_table,
                                 outformat_is_json)
-from globus_cli.services.transfer import get_client, print_json_from_iterator
+from globus_cli.services.transfer import (display_name_or_cname,
+                                          get_client, print_json_from_iterator)
 
 
 @click.command('list', help='List all servers belonging to an Endpoint')
@@ -22,14 +23,14 @@ def server_list(endpoint_id):
 
     if endpoint['host_endpoint_id']:  # not GCS -- this is a share endpoint
         raise click.UsageError(dedent("""\
-            {id} ({display_name}) is a share and does not have servers.
+            {id} ({0}) is a share and does not have servers.
 
             To see details of the share, use
                 globus endpoint show {id}
 
             To list the servers on the share's host endpoint, use
                 globus endpoint server list {host_endpoint_id}
-        """).format(**endpoint.data))
+        """).format(display_name_or_cname(endpoint), **endpoint.data))
 
     if endpoint['s3_url']:  # not GCS -- this is an S3 endpoint
         if outformat_is_json():
