600492881b83b87104136224b99b2ae9845c7853
Aaron Schaer <aaschaer@uchicago.edu>
2017-03-15 13:45:20 -0500

    Remove bookmark locate and make it the default behavior of bookmark show

--- globus_cli/commands/bookmark/show.py
+++ globus_cli/commands/bookmark/show.py
@@ -1,14 +1,17 @@
 import click
 
+from globus_cli.safeio import safeprint
 from globus_cli.parsing import common_options
 from globus_cli.helpers import (
-    outformat_is_json, print_json_response, colon_formatted_print)
+    outformat_is_json, print_json_response, is_verbose, colon_formatted_print)
 
 from globus_cli.services.transfer import get_client
 from globus_cli.commands.bookmark.helpers import resolve_id_or_name
 
 
-@click.command('show', help='Show a Bookmark by either name or ID')
+@click.command(
+    'show', help=("Given a bookmark name or ID resolves bookmark in endpoint:"
+                  "path format. Use --verbose for additional fields."))
 @common_options
 @click.argument('bookmark_id_or_name')
 def bookmark_show(bookmark_id_or_name):
@@ -20,7 +23,13 @@ def bookmark_show(bookmark_id_or_name):
 
     if outformat_is_json():
         print_json_response(res)
-    else:
+
+    # verbose output includes all fields
+    elif is_verbose():
         fields = (('ID', 'id'), ('Name', 'name'),
                   ('Endpoint ID', 'endpoint_id'), ('Path', 'path'))
         colon_formatted_print(res, fields)
+
+    # standard output is endpoint:path format
+    else:
+        safeprint('{}:{}'.format(res['endpoint_id'], res['path']))
