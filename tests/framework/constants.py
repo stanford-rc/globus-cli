d8d6679ff38c4cf2aaee26435094469e179ba4c8
aaschaer <aaschaer@uchicago.edu>
2017-04-06 13:35:15 -0500

    New test refresh tokens

--- tests/framework/constants.py
+++ tests/framework/constants.py
@@ -5,11 +5,11 @@ GO_EP2_ID = "ddb59af0-6d04-11e5-ba46-22000b92c6ec"
 # tokens #
 # ------ #
 CLITESTER1A_TRANSFER_RT = (
-    "AQEAAAAAAASZW3DsgHBmLpsSmnI5ZtmhXOhHaovMnd"
-    "1Ehs_H0h7qy4dn_82sKlSFrJ6Imo_1iWT_YFFD4dmm")
+    "AQEAAAAAAASZW75F06PMlmWcsINaQOKnbY72CBk0Ig"
+    "FSgL3tO6szYUEBJOq8xTDdKtHUKD4o4Pdm9x012Owt")
 CLITESTER1A_AUTH_RT = (
-    "AQEAAAAAAASZXM-UJaOOO0eBAmfVDGAQxezLdkF5Nf"
-    "gcUG6-DRtreGA--ctzQQAI13ClmHxPLH-_Jc5xabRK")
+    "AQEAAAAAAASZXC-lkxvvazq0ZZMiOMOiWHmgmUXrhH"
+    "sS57LzjtA1fNjQYZgS7Mk0mRPRQ4nnpNVdR3D3ts51")
 # ---------- #
 # end tokens #
 # ---------- #
