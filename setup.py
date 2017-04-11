8dc18d68e96aea8e3e4291577fd76414428d0eac
Stephen Rosen <sirosen@uchicago.edu>
2017-04-11 09:32:17 -0500

    Depend on six>=1.10.0 to ensure healthy SDK
    This is all about handling pypa/pip#988
    Resolves #265

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'jmespath==0.9.2',
         'configobj>=5.0.6,<6.0.0',
         'requests>=2.0.0,<3.0.0',
-        'six>=1.1.0,<2.0.0'
+        'six>=1.10.0,<2.0.0'
     ],
 
     entry_points={
