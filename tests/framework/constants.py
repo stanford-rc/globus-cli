663db093d0ec31aeedccfbbceb81f9646ed7dbb3
aaschaer <aaschaer@uchicago.edu>
2017-05-11 16:32:22 -0500

    Add delegate proxy unit tests

--- tests/framework/constants.py
+++ tests/framework/constants.py
@@ -13,3 +13,21 @@ CLITESTER1A_AUTH_RT = (
 # ---------- #
 # end tokens #
 # ---------- #
+
+# public key from an activation requirements response from a GCS endpoint
+PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2zDaVGKy2QpXCUhjEqZP
+raMY4HffgVnRghJLCTEjHYvIYljXHmASjC5k8NbtAf50tVQk8N3rckkpPyFyMhaQ
++Z+s7ANjTOhFwqB//X3JvsPRj7gwN2voiFPXGVveUNCF6bIwfyvKLKmkFXXJxoe8
+cme1Xa2hxkv8n0i9qqIXHOv1RGGnO24I0h9KEJF1LlUP01UPKx+xV/rC4wHHLDih
+RnNdSB1d3vJrpDQB87BGXzYlt8fLbfwG/7r9xrgursFF3lozCwvryy07WFPACh5Y
+lJwyRay/VNRUp+XsKJv6CRJznhTVqzzfKtJItRnlNZSjJTgYQwhYx0uLjo1gBg6t
+p8qTwz/qK0RfJJVWRRMJvEcTUZu6eO4mADVxeimiNy+3wsrnAZjIdxxPPECwsGN7
+W2PeX+NSBL2Z6zsY517098rBDsp4PQ+KkGkzgS5srxM5wVqhPsAjiyhSNTuprWag
+I20HxOotZCW8Gdt4HtbvLYL6ozeTCyV8fZGnGZnjMUHUN5Rz0gemsdgk4sHH1YdM
+28CwgJNYu5/eDtYkxuUtteLwup7Di8Sp8pNDz7iWDhGGWphfeU+tDHc33ul9XPuy
+V/uoKG4hDu4l/bp3wOesH/44IdR12CBqn9Gx1S9FqO9HIM2e07rvDfL2xqvoR5eQ
+kP7mudDcfiovwQRvUawQ/HsCAwEAAQ==
+-----END PUBLIC KEY-----
+"""
