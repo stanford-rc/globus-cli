4fa0bcbae0336517f24207501d5ac048201c49d9
aaschaer <aaschaer@uchicago.edu>
2017-06-26 11:02:36 -0500

    patch get_config_obj in test suite to prevent actual changes to config file

--- tests/manual_cleanup.py
+++ tests/manual_cleanup.py
@@ -1,7 +1,10 @@
-from globus_cli.config import get_config_obj
 from globus_cli.services.transfer import get_client as get_tc
+from tests.framework.cli_testcase import default_test_config
 
-from tests.framework.cli_testcase import write_test_config
+try:
+    from mock import patch
+except ImportError:
+    from unittest.mock import patch
 
 
 def cleanup_bookmarks(tc):
@@ -9,16 +12,10 @@ def cleanup_bookmarks(tc):
         tc.delete_bookmark(bm['id'])
 
 
+@patch("globus_cli.config.get_config_obj", new=default_test_config)
 def main():
-    conf = get_config_obj()
-    stored_conf = conf['cli']
-    write_test_config(conf)
-    try:
-        tc = get_tc()
-        cleanup_bookmarks(tc)
-    finally:
-        conf['cli'] = stored_conf
-        conf.write()
+    tc = get_tc()
+    cleanup_bookmarks(tc)
 
 
 if __name__ == '__main__':
