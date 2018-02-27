cb71d95b06c5686b5b20bf5572cfbb161b17a9ca
Stephen Rosen <sirosen@uchicago.edu>
2018-02-27 11:19:33 -0500

    Fix bugs with --notify omission
    Two minor bugs with `--notify`:
    - When not passed, was producing "None", rather than "{}"
    - `--notify ""` should be the same as `--notify "off"`, so the None
      checking logic has to be explicit, not truthiness check

--- globus_cli/parsing/shared_options.py
+++ globus_cli/parsing/shared_options.py
@@ -332,8 +332,10 @@ def task_submission_options(f):
 
         In code, produces True, False, or a set
         """
-        if not value:
-            return None
+        # if no value was set, don't set any explicit options
+        # the API default is "everything on"
+        if value is None:
+            return {}
 
         value = value.lower()
         value = [x.strip() for x in value.split(',')]
