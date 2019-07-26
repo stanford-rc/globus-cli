9667f645e3b46ddb57049cc29a01a767798c4c3a
Stephen Rosen <sirosen@globus.org>
2019-07-26 19:28:59 +0000

    Fix path normalization on Windows
    closes #455
    
    batchmode transfers and deletes from a Windows system should still
    produce POSX-style paths. Otherwise, these will join incorrectly on
    Windows and then be passed to Transfer where they will be treated as
    POSIX paths. In order to handle this nicely without much headache,
    simply define our own normpath and pathjoin functions based on the
    posixpath implementation in the python stdlib.
    
    This should *NOT* be thought of as "POSIX normalization", but rather
    "Globus Transfer normalization". While in practice those two may be the
    same, there are in theory subtle differences.
    
    For example, this implementation takes the liberty of not caring about
    the number of leading slashes on a path, on the assumption that this can
    hardly even (if ever) matter to Globus Transfer.
    
    Testsuite changes:
    
    Now that #455 is being fixed, the specific tweaking we did to make tests
    pass on Windows should be undone.
    
    Using the dry-run delete tests, add another case with some weirder paths
    (leading slash, `/./`, and `..` use), and make sure that when joined
    with the given base path and normalized, the expected values show up.

--- globus_cli/parsing/task_path.py
+++ globus_cli/parsing/task_path.py
@@ -1,6 +1,51 @@
-import os
-
 import click
+
+
+def _normpath(path):
+    """
+    Globus Transfer-specific normalization, based on a careful reading of the stdlib
+    posixpath implementation:
+      https://github.com/python/cpython/blob/ea0f7aa47c5d2e58dc99314508172f0523e144c6/Lib/posixpath.py#L338
+
+    this must be done without using os.path.normpath to be compatible with CLI calls
+    from Windows systems
+
+    Transfer requires forward slashes, even when communicating with Windows systems, so
+    we must handle these strings appropriately.
+    Note that this does not preserve leading slashes in the same way as python's
+    posixpath module -- it's not clear how Transfer would treat such paths and
+    non-obvious that we need to allow such usage
+    """
+    initial_slash = 1 if path.startswith("/") else 0
+    parts = path.split("/")
+    new_parts = []
+    for part in parts:
+        if part in ("", "."):
+            continue
+        # either not adding a ".." OR chaining together multiple ".."s
+        if part != ".." or (new_parts and new_parts[-1] == ".."):
+            new_parts.append(part)
+        elif new_parts:  # adding a ".." to a path which isn't already ending in one
+            new_parts.pop()
+
+    return ("/" * initial_slash) + "/".join(new_parts)
+
+
+def _pathjoin(a, b):
+    """
+    POSIX-like path join for Globus Transfer paths
+
+    As with _normpath above, this is meant to behave correctly even on Windows systems
+    """
+    if not b:  # given "" as a file path
+        return a
+    elif b.startswith("/"):  # a path starting with / is absolute
+        return b
+
+    if a.endswith("/"):
+        return a + b
+    else:
+        return a + "/" + b
 
 
 class TaskPath(click.ParamType):
@@ -36,11 +81,11 @@ class TaskPath(click.ParamType):
         self.orig_path = self.path = value
 
         if self.base_dir:
-            self.path = os.path.join(self.base_dir, self.path)
+            self.path = _pathjoin(self.base_dir, self.path)
         if self.coerce_to_dir and not self.path.endswith("/"):
             self.path += "/"
         if self.normalize:
-            self.path = os.path.normpath(self.path)
+            self.path = _normpath(self.path)
 
         if self.require_absolute and not (
             self.path.startswith("/") or self.path.startswith("~")
