683a66913a6cab421fc0384e1466da8141be5fdd
Stephen Rosen <sirosen@globus.org>
2020-03-05 22:20:41 +0000

    Convert from LazyIdentityMap to sdk IdentityMap
    Update to sdk v1.9.0 and start using IdentityMap, obsoleting
    LazyIdentityMap and removing it.

--- globus_cli/commands/endpoint/permission/list.py
+++ globus_cli/commands/endpoint/permission/list.py
@@ -1,6 +1,8 @@
+from globus_sdk import IdentityMap
+
 from globus_cli.parsing import command, endpoint_id_arg
 from globus_cli.safeio import formatted_print
-from globus_cli.services.auth import LazyIdentityMap
+from globus_cli.services.auth import get_auth_client
 from globus_cli.services.transfer import get_client
 
 
@@ -12,21 +14,21 @@ def list_command(endpoint_id):
 
     rules = client.endpoint_acl_list(endpoint_id)
 
-    resolved_ids = LazyIdentityMap(
-        x["principal"] for x in rules if x["principal_type"] == "identity"
+    resolved_ids = IdentityMap(
+        get_auth_client(),
+        (x["principal"] for x in rules if x["principal_type"] == "identity"),
     )
 
     def principal_str(rule):
         principal = rule["principal"]
         if rule["principal_type"] == "identity":
-            username = resolved_ids.get(principal)
-            return username or principal
-        elif rule["principal_type"] == "group":
+            try:
+                return resolved_ids[principal]["username"]
+            except KeyError:
+                return principal
+        if rule["principal_type"] == "group":
             return (u"https://app.globus.org/groups/{}").format(principal)
-        else:
-            principal = rule["principal_type"]
-
-        return principal
+        return rule["principal_type"]
 
     formatted_print(
         rules,
