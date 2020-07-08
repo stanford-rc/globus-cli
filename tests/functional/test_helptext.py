c2de882e3e26b0a13226b70d0f3ab87e6b02a112
Stephen Rosen <sirosen@globus.org>
2020-07-08 18:31:02 +0000

    Make security_principal_opts decorator use `wraps`
    Failing to apply functools.wraps results in this decorator not passing
    through attributes of the decorated function. As a result, `__doc__`
    (which becomes helptext) is not passed through and the help text is
    not properly displayed.
    
    fixes #496

---
+++ tests/functional/test_helptext.py
@@ -0,0 +1,17 @@
+"""
+tests which ensure that helptext rendering is correct
+"""
+
+
+# This is a regression test for
+#   https://github.com/globus/globus-cli/issues/496
+def test_helptext_for_commands_with_security_principal_opts(run_line):
+    """
+    Test commands which use the security principal options and ensure that their
+    helptext renders correctly.
+    """
+    result = run_line("globus endpoint role create --help")
+    assert "Create a role on an endpoint" in result.output
+
+    result = run_line("globus endpoint permission create --help")
+    assert "Create an access control rule, allowing new permissions" in result.output
