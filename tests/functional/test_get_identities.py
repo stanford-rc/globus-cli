67f1b706215be5960bd9e3e99b00a1c5550cb9e4
Stephen Rosen <sirosen@globus.org>
2021-01-15 00:23:20 +0000

    Convert get-identities tests to responses

--- tests/functional/test_get_identities.py
+++ tests/functional/test_get_identities.py
@@ -1,72 +1,82 @@
 import json
 
 
-def test_default_one_id(run_line, user_data):
+def test_default_one_id(run_line, load_api_fixtures):
     """
     Runs get-identities with one id, confirms correct username returned
     """
-    result = run_line("globus get-identities " + user_data["clitester1a"]["id"])
-    assert user_data["clitester1a"]["username"] + "\n" == result.output
+    data = load_api_fixtures("foo_user_info.yaml")
+    user_id = data["metadata"]["user_id"]
+    username = data["metadata"]["username"]
+    result = run_line("globus get-identities {}".format(user_id))
+    assert username + "\n" == result.output
 
 
-def test_defualt_one_username(run_line, user_data):
+def test_defualt_one_username(run_line, load_api_fixtures):
     """
     Runs get-identities with one username, confirms correct id returned
     """
-    result = run_line("globus get-identities " + user_data["clitester1a"]["username"])
-    assert user_data["clitester1a"]["id"] + "\n" == result.output
+    data = load_api_fixtures("foo_user_info.yaml")
+    user_id = data["metadata"]["user_id"]
+    username = data["metadata"]["username"]
+    result = run_line("globus get-identities " + username)
+    assert user_id + "\n" == result.output
 
 
-def test_default_invalid(run_line, user_data):
+def test_default_invalid(run_line, register_api_route):
     """
     Runs get-identities with one username, confirms correct id returned
     """
+    register_api_route("auth", "/v2/api/identities", json={"identities": []})
     result = run_line("globus get-identities invalid")
     assert "NO_SUCH_IDENTITY\n" == result.output
 
 
-def test_default_multiple_inputs(run_line, user_data):
+def test_default_multiple_inputs(run_line, load_api_fixtures):
     """
     Runs get-identities with id username, duplicate and invalid inputs
     Confirms order is preserved and all values are as expected
     """
+    data = load_api_fixtures("multiuser_get_identities.yaml")
+    users = data["metadata"]["users"]
     in_vals = [
-        user_data["clitester1a"]["username"],
-        user_data["clitester1a"]["id"],
+        users[0]["username"],
+        users[0]["user_id"],
         "invalid",
-        user_data["go"]["username"],
-        user_data["go"]["username"],
+        users[1]["username"],
+        users[1]["username"],
     ]
 
     expected = [
-        user_data["clitester1a"]["id"],
-        user_data["clitester1a"]["username"],
+        users[0]["user_id"],
+        users[0]["username"],
         "NO_SUCH_IDENTITY",
-        user_data["go"]["id"],
-        user_data["go"]["id"],
+        users[1]["user_id"],
+        users[1]["user_id"],
     ]
 
     result = run_line("globus get-identities " + " ".join(in_vals))
     assert "\n".join(expected) + "\n" == result.output
 
 
-def test_verbose(run_line, user_data):
+def test_verbose(run_line, load_api_fixtures):
     """
     Runs get-identities with --verbose, confirms expected fields found
     """
-    go_data = user_data["go"]
-    result = run_line("globus get-identities --verbose " + go_data["id"])
-    for key in ["username", "id", "name", "organization", "email"]:
-        assert go_data[key] in result.output
+    data = load_api_fixtures("foo_user_info.yaml")
+    user_id = data["metadata"]["user_id"]
+    result = run_line("globus get-identities --verbose " + user_id)
+    for key in ["username", "user_id", "name", "organization", "email"]:
+        assert data["metadata"][key] in result.output
 
 
-def test_json(run_line, user_data):
+def test_json(run_line, load_api_fixtures):
     """
     Runs get-identities with -F json confirms expected values
     """
-    go_data = user_data["go"]
-    output = json.loads(
-        run_line("globus get-identities -F json " + go_data["id"]).output
-    )
-    for key in go_data:
-        assert go_data[key] in output["identities"][0][key]
+    data = load_api_fixtures("foo_user_info.yaml")
+    user_id = data["metadata"]["user_id"]
+    output = json.loads(run_line("globus get-identities -F json " + user_id).output)
+    assert data["metadata"]["user_id"] == output["identities"][0]["id"]
+    for key in ["username", "name", "organization", "email"]:
+        assert data["metadata"][key] == output["identities"][0][key]
