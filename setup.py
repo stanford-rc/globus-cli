016b9512d2fb174862d7be05d413f08d9add5efd
Stephen Rosen <sirosen@globus.org>
2021-06-01 19:07:54 +0000

    Convert to `src/` tree layout

--- setup.py
+++ setup.py
@@ -1,17 +1,50 @@
 import os
+import re
 
 from setuptools import find_packages, setup
 
-# single source of truth for package version
-version_ns = {}  # type: ignore
-with open(os.path.join("globus_cli", "version.py")) as f:
-    exec(f.read(), version_ns)
-version = version_ns["__version__"]
+DEV_REQUIREMENTS = [
+    # lint
+    "flake8<4",
+    "isort<6",
+    "black==21.5b1",
+    "flake8-bugbear==21.4.3",
+    "mypy==0.812",
+    # tests
+    "pytest<7",
+    "pytest-cov<3",
+    "pytest-xdist<3",
+    "responses==0.13.3",
+    # loading test fixture data
+    "ruamel.yaml==0.16.12",
+]
+
+
+def parse_version():
+    # single source of truth for package version
+    version_string = ""
+    version_pattern = re.compile(r'__version__ = "([^"]*)"')
+    with open(os.path.join("src", "globus_cli", "version.py")) as f:
+        for line in f:
+            match = version_pattern.match(line)
+            if match:
+                version_string = match.group(1)
+                break
+    if not version_string:
+        raise RuntimeError("Failed to parse version information")
+    return version_string
+
+
+def read_readme():
+    with open("README.rst") as fp:
+        return fp.read()
+
 
 setup(
     name="globus-cli",
-    version=version,
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    version=parse_version(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     python_requires=">=3.6",
     install_requires=[
         "globus-sdk==2.0.1",
@@ -27,28 +60,11 @@ setup(
         # this requirement
         "cryptography>=1.8.1,<3.4.0",
     ],
-    extras_require={
-        # the development extra is for CLI developers only
-        "development": [
-            # testing
-            "pytest<5",
-            "pytest-cov<3.0",
-            # mocking `requests`
-            "responses==0.12.1",
-            # loading fixture data
-            "ruamel.yaml==0.16.12",
-            # linting tools
-            "black==20.8b1",
-            "isort>=5.6.4,<6.0",
-            "flake8>=3.8.4,<4.0",
-            "flake8-bugbear==20.11.1",
-            "mypy==0.800",
-        ],
-    },
+    extras_require={"development": DEV_REQUIREMENTS},
     entry_points={"console_scripts": ["globus = globus_cli:main"]},
     # descriptive info, non-critical
     description="Globus CLI",
-    long_description=open("README.rst").read(),
+    long_description=read_readme(),
     author="Stephen Rosen",
     author_email="sirosen@globus.org",
     url="https://github.com/globus/globus-cli",
