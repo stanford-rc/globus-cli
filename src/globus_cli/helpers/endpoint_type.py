c4fe39783cc6ca25eb029289d98790894efdf45b
Aaron Schaer <aaschaer@globus.org>
2021-08-23 15:31:30 -0500

    make determine_endpoint_type a classmethod

--- src/globus_cli/helpers/endpoint_type.py
+++ src/globus_cli/helpers/endpoint_type.py
@@ -10,33 +10,33 @@ class EndpointType(Enum):
     SHARE = auto()
     NON_GCSV5_ENDPOINT = auto()  # most likely GCSv4, but not necessarily
 
+    @classmethod
+    def determine_endpoint_type(cls, ep_doc: dict) -> "EndpointType":
+        """
+        Given an endpoint document from transfer, determine what type of
+        endpoint or collection it is for
+        """
+        if ep_doc["is_globus_connect"] is True:
+            return EndpointType.GCP
 
-def determine_endpoint_type(ep_doc: dict) -> EndpointType:
-    """
-    Given an endpoint document from transfer, determine what type of
-    endpoint or collection it is for
-    """
-    if ep_doc["is_globus_connect"] is True:
-        return EndpointType.GCP
+        if ep_doc["non_functional"] is True:
+            return EndpointType.GCSV5_ENDPOINT
 
-    if ep_doc["non_functional"] is True:
-        return EndpointType.GCSV5_ENDPOINT
+        shared = ep_doc["host_endpoint_id"] is not None
 
-    shared = ep_doc["host_endpoint_id"] is not None
-
-    if ep_doc["gcs_version"]:
-        major, minor, patch = ep_doc["gcs_version"].split(".")
-        gcsv5 = major == "5"
-    else:
-        gcsv5 = False
-
-    if gcsv5:
-        if shared:
-            return EndpointType.GUEST_COLLECTION
+        if ep_doc["gcs_version"]:
+            major, minor, patch = ep_doc["gcs_version"].split(".")
+            gcsv5 = major == "5"
         else:
-            return EndpointType.MAPPED_COLLECTION
+            gcsv5 = False
 
-    elif shared:
-        return EndpointType.SHARE
+        if gcsv5:
+            if shared:
+                return EndpointType.GUEST_COLLECTION
+            else:
+                return EndpointType.MAPPED_COLLECTION
 
-    return EndpointType.NON_GCSV5_ENDPOINT
+        elif shared:
+            return EndpointType.SHARE
+
+        return EndpointType.NON_GCSV5_ENDPOINT
