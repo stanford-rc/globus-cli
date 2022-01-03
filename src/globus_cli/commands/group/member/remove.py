6fab1300bf36c52e7694f00ba2c838155198f579
Rudyard Richter <rudyardrichter@uchicago.edu>
2022-01-03 15:41:09 -0600

    Add `group member remove` command

--- src/globus_cli/commands/group/member/remove.py
+++ src/globus_cli/commands/group/member/remove.py
@@ -5,20 +5,20 @@ from globus_cli.parsing import IdentityType, ParsedIdentity, command
 from globus_cli.termio import FORMAT_TEXT_RECORD, formatted_print
 from globus_cli.types import FIELD_LIST_T
 
-ADD_USER_FIELDS: FIELD_LIST_T = [
+REMOVED_USER_FIELDS: FIELD_LIST_T = [
     ("Group ID", "group_id"),
-    ("Added User ID", "identity_id"),
-    ("Added User Username", "username"),
+    ("Removed User ID", "identity_id"),
+    ("Removed User Username", "username"),
 ]
 
 
-@command("add", short_help="Add a member to a group")
+@command("remove", short_help="Remove a member from a group")
 @click.argument("group_id", type=click.UUID)
 @click.argument("user", type=IdentityType())
 @LoginManager.requires_login(LoginManager.GROUPS_RS)
-def member_add(group_id: str, user: ParsedIdentity, login_manager):
+def member_remove(group_id: str, user: ParsedIdentity, login_manager):
     """
-    Add a member to a group.
+    Remove a member from a group.
 
     The USER argument may be an identity ID or username (whereas the group must be
     specified with an ID).
@@ -28,17 +28,16 @@ def member_add(group_id: str, user: ParsedIdentity, login_manager):
     identity_id = auth_client.maybe_lookup_identity_id(user.value)
     if not identity_id:
         raise click.UsageError(f"Couldn't determine identity from user value: {user}")
-    actions = {"add": [{"identity_id": identity_id}]}
+    actions = {"remove": [{"identity_id": identity_id}]}
     response = groups_client.batch_membership_action(group_id, actions)
-    # If this call failed to return an added user, figure out an error to show
-    if not response.get("add", None):
+    if not response.get("remove", None):
         try:
-            raise ValueError(response["errors"]["add"][0]["detail"])
+            raise ValueError(response["errors"]["remove"][0]["detail"])
         except (IndexError, KeyError):
-            raise ValueError("Could not add user to group")
+            raise ValueError("Could not remove the user from the group")
     formatted_print(
         response,
         text_format=FORMAT_TEXT_RECORD,
-        fields=ADD_USER_FIELDS,
-        response_key=lambda data: data["add"][0],
+        fields=REMOVED_USER_FIELDS,
+        response_key=lambda data: data["remove"][0],
     )
